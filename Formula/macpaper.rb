0386c25c486f40da13208d794969bc762598ff6c
Anant Patel <66427020+anantdark@users.noreply.github.com>
2026-08-04 04:15:40 +0530

    Set Homebrew formula sha256 for v1.4.1.

--- Formula/macpaper.rb
+++ Formula/macpaper.rb
@@ -2,7 +2,7 @@ class Macpaper < Formula
   desc "Register local videos and images in macOS Wallpaper and Screen Saver"
   homepage "https://github.com/anantdark/macpaper"
   url "https://github.com/anantdark/macpaper/archive/refs/tags/v1.4.1.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "cc0f36e000560fb1d80c44587eaeed610f8788c0fe8226040222e2028cc4bc88"
   license "GPL-3.0-or-later"
   version "1.4.1"
 
